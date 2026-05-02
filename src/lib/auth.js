1e1d40fcc87c2d807da23a8df5e481614931f7a3
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-05-02 22:11:07 +0600

    added google auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,13 +3,18 @@ import { MongoClient } from 'mongodb';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("SKILL_SPHERE");
+const db = client.db('SKILL_SPHERE');
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    client,
-  }),
+  database: mongodbAdapter(db, { client }),
   emailAndPassword: {
     enabled: true,
-  }
+  },
+  // ✅ এটা add করো
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 });
