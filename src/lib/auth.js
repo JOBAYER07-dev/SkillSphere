8849fd737bbcda781434335fa72392215b4edebd
JOBAYER07-dev <jobayerhosen045@gmail.com>
2026-05-03 01:24:06 +0600

    fixed google login isse

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,14 +3,26 @@ import { MongoClient } from 'mongodb';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 
 const client = new MongoClient(process.env.MONGODB_URI);
+
+
+if (!client.topology?.isConnected()) {
+  await client.connect();
+}
+
 const db = client.db('SKILL_SPHERE');
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, { client }),
+
+  trustedOrigins: [
+    'http://localhost:3000',
+    'https://skill-sphere-sable.vercel.app',
+  ],
+
   emailAndPassword: {
     enabled: true,
   },
-  // ✅ এটা add করো
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
